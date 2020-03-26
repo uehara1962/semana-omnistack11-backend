3d83d0af846c491e9c84e4cccbd5641e22815e6f
Carlos Uehara <ca_uehara@hotmail.com>
2020-03-25 21:12:31 -0300

    third commit

--- knexfile.js
+++ knexfile.js
@@ -34,12 +34,13 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME
-    },
+    // connection: {
+    //   host: process.env.DB_HOST,
+    //   user: process.env.DB_USER,
+    //   password: process.env.DB_PASS,
+    //   database: process.env.DB_NAME
+    // },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
